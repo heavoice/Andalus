45ef44b117b3c8f77f00da6365415bd3c42d0c4d
wapiiqo <muhammadwafiq1313@gmail.com>
2025-03-14 10:14:48 +0700

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import About from "@/components/About";
-import Feed from "@/components/Feed";
 
 export default function LandingPage() {
   return (
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Image from "next/image";
-
 const Feed = () => {
   return <div className="bg-black w-full h-screen relative"></div>;
 };
